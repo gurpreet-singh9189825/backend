7041feeb7bf329ab713615d7a1f9e9f0635d6b12
gurpreet-singh9189825 <gs9189825@gmail.com>
2025-02-05 20:35:29 -0800

    connect db

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+const connectDB = async () => {
+    try {
+      if (!process.env.MONGODB_URI) {
+        throw new Error("MONGODB_URI is not defined in .env file");
+      }
+      
+      
+      const connectionInstance = await mongoose.connect(
+        `${process.env.MONGODB_URI}/${DB_NAME}`
+      );
+  
+      console.log(`\n MongoDB connected! DB Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+      console.error("MongoDB connection error:", error.message);
+      process.exit(1);
+    }
+  };
+export default connectDB  
